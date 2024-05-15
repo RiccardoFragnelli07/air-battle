f21d17310f161200ec9f7ec6ef7b8720c72a441e
matthew-prime <matthew.penati771@gmail.com>
2024-05-15 13:49:26 +0200

    rotazione jet

--- airbattle/aereo.py
+++ airbattle/aereo.py
@@ -3,8 +3,10 @@ import math
 from funzioni import distanza_punti
 
 VEL = 7
+ROTAZIONE = 0.4
 HEIGHT = 690
 WIDTH = 500
+IND = 0
 
 class Aereo:
     def __init__(self, rect, img, img_effetti, screen, time = 0):
@@ -12,6 +14,7 @@ class Aereo:
         self.img = img
         self.screen = screen
         self.time = time
+        self.ind = 0
         self.effetti = []
         effetti_width = img_effetti.get_width() // 8
         effetti_height = img_effetti.get_height()
@@ -21,7 +24,17 @@ class Aereo:
             self.effetti.append(img_part)
         self.centro_aereo = pygame.math.Vector2(self.rect.x + self.rect.width, self.rect.y + self.rect.height)
         
-    def move(self, key, pos):
+        self.jet = []
+        jet_width = self.img.get_width() // 60
+        jet_height = self.img.get_height()
+        for i in range(60):
+            startx = i * jet_width
+            img_part = self.img.subsurface((startx, 0, jet_width, jet_height))
+            img_part = pygame.transform.scale(img_part, (100, 100))
+            self.jet.append(img_part)
+            
+        
+    def move(self, key, lasc, pos):
         if key[pygame.K_w] and self.rect.y - VEL >= 0:
             self.rect.y -= VEL  
         if key[pygame.K_s] and self.rect.y + VEL <= HEIGHT-100:
@@ -30,10 +43,23 @@ class Aereo:
             self.rect.x -= VEL
         if key[pygame.K_d] and self.rect.x + VEL <= WIDTH-100:
             self.rect.x += VEL
+        if key[pygame.K_LEFT]:
+            self.ind -= ROTAZIONE
+            if self.ind <= -60:
+                self.ind = 0
+        if key[pygame.K_RIGHT]:
+            self.ind += ROTAZIONE
+            if self.ind >= 60:
+                self.ind = 0
+        if lasc == pygame.K_RIGHT or lasc == pygame.K_LEFT:
+            self.ind = 0
+        
+            
+            
         # ipo = distanza_punti(self.centro_aereo, pos)
         # catx = distanza_punti()
         
     def draw(self, screen):
-        screen.blit(self.img, (self.rect.x, self.rect.y))
+        screen.blit(self.jet[int(self.ind)], (self.rect.x, self.rect.y))
         screen.blit(self.effetti[int(self.time) % 8], (self.rect.x, self.rect.y + 2))
         self.time += 0.2
--- airbattle/mainairbattle.py
+++ airbattle/mainairbattle.py
@@ -5,7 +5,7 @@ from aereo import Aereo
 pygame.init()
 
 WIDTH, HEIGHT = 500, 700
-WHITE = (250,250,250)
+WHITE = (0,0,0)
 VEL = 7
 
 
@@ -13,18 +13,23 @@ def carica_texture(immagini = []):
     immagini.append([])
     immagini.append([])
     
-    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF01a_strip60.png"))
-    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF02a_strip60.png"))
-    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF04a_strip60.png"))
-    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF03a_strip60.png"))
+    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF01.png"))
+    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF02.png"))
+    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF04.png"))
+    immagini[0].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF03.png"))
     
     immagini[1].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF01a_strip60.png"))
     immagini[1].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF02a_strip60.png"))
     immagini[1].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF04a_strip60.png"))
     immagini[1].append(pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Jet\\SF03a_strip60.png"))
 
+def Draw(aereo):
+    screen.fill(WHITE)
+    screen.blit(img_aereo, (aereoPri.x, aereoPri.y))
+    pygame.display.update()
 
-aereo_x, aereo_y, dim_aereo_x, dim_aereo_y  = 150, 545, 100, 100
+# aereo_x, aereo_y, dim_aereo_x, dim_aereo_y  = 150, 545, 100, 100
+aereo_x, aereo_y, dim_aereo_x, dim_aereo_y  = 150, 545, 14400, 240
 proiettile_x, proiettile_y, dim_proiettile_x, dim_proiettile_y = 50, 50, 800, 100
 aereo_rect = pygame.Rect(aereo_x, aereo_y, dim_aereo_x, dim_aereo_y)
 effetti_rect = pygame.Rect(proiettile_x, proiettile_y, dim_proiettile_x, dim_proiettile_y)
@@ -34,22 +39,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 pygame.display.set_caption("AIR BATTLE")
-img_aereo = pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Navicelle\\Designs - Base\\PNGs\\Nairan - Torpedo Ship - Base.png")
+# img_aereo = pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Navicelle\\Designs - Base\\PNGs\\Nairan - Torpedo Ship - Base.png")
 img_effetti = pygame.image.load("C:\\Users\\Matteo\\Desktop\\Pygame\\Videogioco\\Textures\\Navicelle\\Engine Effects\\PNGs\\Nairan - Torpedo Ship - Engine.png")
 jet_texture = []
 carica_texture(jet_texture)
 
-img_aereo = pygame.transform.scale(img_aereo, (dim_aereo_x, dim_aereo_y))
+img_aereo = pygame.transform.scale(jet_texture[1][2], (dim_aereo_x, dim_aereo_y))
 img_effetti = pygame.transform.scale(img_effetti, (dim_proiettile_x, dim_proiettile_y))
 aereoPri = pygame.Rect(aereo_x, aereo_y, dim_aereo_x, dim_aereo_y)
 effettiPri = pygame.Rect(proiettile_x, proiettile_y, dim_proiettile_x, dim_proiettile_y)
 aereo = Aereo(aereo_rect, img_aereo, img_effetti, screen)
+tasto_lasciato = None
 
-
-def Draw(aereo):
-    screen.fill(WHITE)
-    screen.blit(img_aereo, (aereoPri.x, aereoPri.y))
-    pygame.display.update()
 
 
 FPS = 60
@@ -62,9 +63,12 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYUP:
+            tasto_lasciato = pygame.key.name(event.key)
+            print(f"tasto lasciato: {tasto_lasciato}\n")
     
     key_pressed = pygame.key.get_pressed()
-    aereo.move(key_pressed, pygame.math.Vector2(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
+    aereo.move(key_pressed, tasto_lasciato, pygame.math.Vector2(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
     
     
     screen.fill(WHITE)
@@ -73,3 +77,7 @@ while run:
     
 
 pygame.quit()
+
+
+
+# carriata da glucozio (10 in pagella per lui obbligatorio)
